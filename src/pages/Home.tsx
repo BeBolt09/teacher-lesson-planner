0e23a995a3791c683d1e6d2024c5c6167e648871
Sebastien Dupont <sebastiengdupont@gmail.com>
2024-02-13 20:46:59 -0800

    Updated Home.TSX
    Added some imgs and video to home

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -2,18 +2,18 @@ import Footer from "../components/Footer"
 function Home() {
   return (
     <>  
-      <div className="bg-gray-200 rounded-lg overflow-hidden shadow-lg w-2/5 border-2 m-40">
-        <video
-          className=""
-          autoPlay
-          loop
-          muted
-          playsInline
-          src="/Lesson Planner Demo .mp4" // Replace with the path to your video
-        />
+    <div className="flex flex-row items-center justify-center">
+      <div className="w-2/5 mx-10">
+        <video autoPlay loop muted playsInline src="/Lesson Planner Demo .mp4" className="bg-gray-200 rounded-lg overflow-hidden shadow-lg border-2 mt-36 mx-10"/>
+        <img src="/Step2.png" className="bg-gray-200 rounded-lg overflow-hidden shadow-lg  border-2 mt-36 mx-10" />
+        <img src="/Step4.png" className="bg-gray-200 rounded-lg overflow-hidden shadow-lg  border-2 mt-36 mx-10" />
       </div>
+      <div className="w-2/5 mx-10">
+        <img src="/Step1.png" className="bg-gray-200 rounded-lg overflow-hidden shadow-lg  border-2 mt-36 mx-10"/>
+        <img src="/Step3.png" className="bg-gray-200 rounded-lg overflow-hidden shadow-lg  border-2 mt-36 mx-10" />
+      </div>
+    </div>
 
-        <div className="mt-44">h</div>
         <div className="mt-44">h</div>
         <div className="mt-44">h</div>
         <div className="mt-44">h</div>
